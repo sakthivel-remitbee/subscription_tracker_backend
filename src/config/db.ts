9a1c34d5ae55a5131003cb2a7a6b832b8a1a4b59
sakthivvel-remitbee <sakthivel.rameshbabu@remitbee.com>
2026-04-05 23:29:12 +0530

    Bc-189 bug fixed  and dev tested

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(process.env.DB_NAME as string,process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
   host: process.env.DB_HOST as string,
   dialect: 'mysql',
+  logging: false,
 });
 
 export default sequelize;
